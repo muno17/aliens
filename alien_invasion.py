9c8e7a6babc34f473434dd5df2b18b607c785397
muno17 <munozd17@gmail.com>
2023-09-21 19:14:54 -0400

    add update() to ship to allow ship to continously move left or right

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,20 +25,34 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
-        # Respond to keypresses and mouse events
+        """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
